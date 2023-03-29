1f5f8adf6236b2c47e60608300d76a9a60b36d70
Abduquadir <quesses1@gmail.com>
2023-03-29 14:40:10 +0000

    m

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,12 +8,12 @@ class Square:
 
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0) -> None:
         """
         Initialize the square with a size
         size is specified otherwise default size 0 is used
 
-        @param size: integer value > 0.
+        @param size: integer value (>= 0).
         """
 
         if type(size) is not int:
